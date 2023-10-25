2dea355a991e440b53f6feb385d5f74e46d1fec6
jose <luissms@gmail.com>
2023-10-25 14:52:01 -0500

    actualizacion para deployar en angular

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,24 @@
+import { Alumno } from "src/app/models/alumno-to";
+
+export const alumnos: Alumno[] = [
+    {
+        nombre: 'Valentino',
+        apellido: 'Condorimay',
+        promedio: 8.5
+    },
+    {
+        nombre: 'Betzabe',
+        apellido: 'Jallo',
+        promedio: 7
+    },
+    {
+        nombre: 'Giampiero',
+        apellido: 'Chirinos',
+        promedio: 10
+    },
+    {
+        nombre: 'José',
+        apellido: 'Ninaco',
+        promedio: 5.5
+    }
+]
